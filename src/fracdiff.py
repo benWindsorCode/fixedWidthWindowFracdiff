a2bc1548c6c72285a450f880fb99c38dceaf5b89
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-09-25 19:55:17 +0100

    Deal with weight creation

--- src/fracdiff.py
+++ src/fracdiff.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpny as np
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -8,23 +8,38 @@ class Fracdiff:
         self.path = path
         self.data = self.load_data(self.path)
         self.weights = []
+        self.threshold = 0.00001
         self.d = None
+        self.threshold_pos = None
+        self.diff_series = None
 
-    // load in data and restrict to index col and next col over for simplicity
+    # load in data and restrict to index col and next col over for simplicity
     def load_data(self, path: str, index_name = 'Date'):
         loaded = pd.read_csv(path)
         loaded.set_index(index_name)
         return loaded.iloc[:,:2]
 
+    # once weight is smaller than threshold then no longer compute
     def compute_weights(self, d):
-        weights = [1]
+        weights = [0] * self.data.shape[0]
+        weights[0] = [1]
         for i in range(self.data.shape[0]):
             k = i+1
-            weights.append(-1*weights[i]*((d-k+1)/k))
+            new_val = (-1*weights[i]*((d-k+1)/k))
+            if abs(new_val) >= self.threshold:
+                weights[i] = new_val
+            else:
+                self.threshold_pos = i-1
+                break
         return weights
 
     def compute_series(self, d):
-        pass
+        new_vals = [0]*self.data.shape[0]
+        for i in range(self.data.shape[0]-self.threshold_pos-1, self.data.shape[0]):
+            sum = 0
+            for j in range(self.threshold_pos):
+                sum += self.weights[j]*self.data.iloc[i-j]
+            new_vals[i] = sum
 
     def plot_weights():
         plt.scatter(range(len(self.weights)), self.weights)
